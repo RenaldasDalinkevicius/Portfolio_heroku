68eed2f2786ce4bf27d3b0dd64548750f5907c8c
KodeRenaldas <95746005+KodeRenaldas@users.noreply.github.com>
2024-08-22 09:10:44 +0200

    conn

--- db/conn.js
+++ db/conn.js
@@ -1,21 +1,17 @@
 import { MongoClient } from "mongodb"
 const Db = process.env.ATLAS_URI
 const DbName = process.env.DB_NAME
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+const client = new MongoClient(Db)
 let _db
-export function connectToServer(callback) {
-    client.connect(function (err, db) {
-      if (db)
-      {
-        _db = db.db("portfolio")
-        console.log("Successfully connected to MongoDB.")
-      }
-      return callback(err)
-         })
-  }
+export async function connectToServer(callback) {
+  await client.connect(function(err, db) {
+    if (db) {
+      _db = db.db("portfolio")
+      console.log("Successfully connected to MongoDB.")
+    }
+    return callback(err)
+  })
+}
 export function getDb() {
     return _db
 }
