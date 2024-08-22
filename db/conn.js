f7a96006bc7203df165a57c29fbead878bfd50f7
KodeRenaldas <95746005+KodeRenaldas@users.noreply.github.com>
2024-08-22 09:28:23 +0200

    conn

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,6 @@
 import { MongoClient } from "mongodb"
 const Db = process.env.ATLAS_URI
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+const client = new MongoClient(Db)
 let _db
 export function connectToServer(callback) {
   client.connect(function (err, db) {
